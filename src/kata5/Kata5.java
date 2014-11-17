967d3112efe3194ca48eefc0fa7e7fae2fad2323
usuario <usuario@10.22.143.19>
2014-11-17 13:45:47 +0000

    Kata5

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -3,7 +3,7 @@ package kata5;
 public class Kata5 {
 
     public static void main(String[] args) {
-        new MoneyCalculatorFrame().execute();
+        new MoneyCalculatorFrame();
     }
     
 }
--- src/kata5/MoneyCalculatorFrame.java
+++ src/kata5/MoneyCalculatorFrame.java
@@ -1,14 +1,34 @@
 package kata5;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.PopupMenu;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 class MoneyCalculatorFrame extends JFrame {
 
     public MoneyCalculatorFrame() {
         setTitle("MoneyCalculator");
         setMinimumSize(new Dimension(300, 300));
-        setDefaultCloseOperation(WIDTH);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createComponents();
+        setVisible(true);
+    }
+
+    private void createComponents() {
+        add(createExchangeDialog());
+        add(createToolbar(), BorderLayout.SOUTH);
+    }
+
+    private Component createExchangeDialog() {
+        JPanel panel = new JPanel(new FlowLayout());
+        return panel;
+    }
+
+    private Component createToolbar() {
     }
     
 }
