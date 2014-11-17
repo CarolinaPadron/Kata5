080fcef186683512b68303cf6e54eec8145a224a
usuario <usuario@10.22.143.19>
2014-11-17 13:38:27 +0000

    Kata5

---
+++ src/kata5/MoneyCalculatorFrame.java
@@ -0,0 +1,13 @@
+package kata5;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+class MoneyCalculatorFrame extends JFrame {
+
+    public MoneyCalculatorFrame() {
+        setTitle("MoneyCalculator");
+        setMinimumSize(new Dimension(WIDTH, WIDTH));
+    }
+    
+}
